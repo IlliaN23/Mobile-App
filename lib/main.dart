3ab5b18c63f9ebcc9439d485683614ae23818b3c
Salss <ssshilov111@gmail.com>
2025-07-13 18:49:51 +0300

    First State

--- lib/main.dart
+++ lib/main.dart
@@ -115,205 +115,202 @@ class _MainPageState extends State<MainPage> {
       ),
     );
   }
+}
 
-  Widget _buildHeader(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Hello, User',
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      color: Colors.black54,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14,
-                    ),
-              ),
-              Text(
-                'Manage your time',
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                      fontSize: 18,
-                    ),
-              ),
-            ],
+Widget _buildHeader(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hello, User',
+              style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                  ),
+            ),
+            Text(
+              'Manage your time',
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontSize: 18,
+                  ),
+            ),
+          ],
+        ),
+        GestureDetector(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => const profilePage.ProfilePage()),
           ),
-          GestureDetector(
-            onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (_) => const profilePage.ProfilePage()),
-            ),
-            child: CircleAvatar(
-              radius: 20,
-              backgroundColor: Colors.blue.shade50,
-              child: const Icon(Icons.person_outline,
-                  color: Colors.blue, size: 20),
-            ),
+          child: CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.blue.shade50,
+            child:
+                const Icon(Icons.person_outline, color: Colors.blue, size: 20),
           ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildDashboardGrid(BuildContext context) {
-    final List<DashboardItem> dashboardItems = [
-      DashboardItem(
-        title: 'To do list',
-        subtitle: 'All the tasks in this project',
-        icon: Icons.task,
-        color: Colors.blue,
-        value: 5,
-        totalValue: 10,
-        onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (_) => const tasksPage.TasksPage()),
-        ),
-      ),
-      DashboardItem(
-        title: 'Vacations',
-        subtitle: 'Vacations C++ Developer',
-        icon: Icons.work,
-        color: Colors.green,
-        value: 3,
-        totalValue: 5,
-        onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (_) => const workPage.WorkPage()),
-        ),
-      ),
-      DashboardItem(
-        title: 'Projects',
-        subtitle: 'Last change',
-        icon: Icons.folder,
-        color: Colors.orange,
-        value: 3,
-        totalValue: 8,
-        onTap: _openProjectFolder,
-      ),
-    ];
-
-    return Expanded(
-      child: GridView.builder(
-        padding: const EdgeInsets.all(16),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 16,
-          mainAxisSpacing: 16,
-          childAspectRatio: 3 / 2,
-        ),
-        itemCount: dashboardItems.length,
-        itemBuilder: (context, index) {
-          final item = dashboardItems[index];
-          return GestureDetector(
-            onTap: item.onTap,
-            child: GlassmorphicContainer(
-              width: double.infinity,
-              height: 120,
-              borderRadius: 16,
-              blur: 8,
-              alignment: Alignment.bottomCenter,
-              border: 1,
-              linearGradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  item.color.withOpacity(0.1),
-                  item.color.withOpacity(0.05),
-                ],
-              ),
-              borderGradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.white.withOpacity(0.5),
-                  Colors.white.withOpacity(0.2),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(item.icon, color: item.color, size: 24),
-                        Text(
-                          '${item.value}/${item.totalValue}',
-                          style: TextStyle(
-                            color: item.color,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          item.title,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          item.subtitle,
-                          style: const TextStyle(
-                            color: Colors.black54,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildBottomNavigation(BuildContext context) {
-    return BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.task, color: Colors.grey.shade600),
-          label: 'Tasks',
-        ),
-        const BottomNavigationBarItem(
-          icon: Icon(Icons.home, color: Colors.blue),
-          label: 'General',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person, color: Colors.grey.shade600),
-          label: 'Profile',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.chat, color: Colors.grey.shade600),
-          label: 'Massanger(L9)',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.folder, color: Colors.grey.shade600),
-          label: 'Projects',
         ),
       ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.blue,
-      onTap: _onItemTapped,
-    );
-  }
+    ),
+  );
+}
+
+Widget _buildDashboardGrid(BuildContext context) {
+  var _openProjectFolder;
+  final List<DashboardItem> dashboardItems = [
+    DashboardItem(
+      title: 'To do list',
+      subtitle: 'All the tasks in this project',
+      icon: Icons.task,
+      color: Colors.blue,
+      value: 5,
+      totalValue: 10,
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => const tasksPage.TasksPage()),
+      ),
+    ),
+    DashboardItem(
+      title: 'Vacations',
+      subtitle: 'Vacations C++ Developer',
+      icon: Icons.work,
+      color: Colors.green,
+      value: 3,
+      totalValue: 5,
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => const workPage.WorkPage()),
+      ),
+    ),
+    DashboardItem(
+      title: 'Projects',
+      subtitle: 'Last change',
+      icon: Icons.folder,
+      color: Colors.orange,
+      value: 3,
+      totalValue: 8,
+      onTap: _openProjectFolder,
+    ),
+  ];
+
+  return Expanded(
+    child: GridView.builder(
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        childAspectRatio: 3 / 2,
+      ),
+      itemCount: dashboardItems.length,
+      itemBuilder: (context, index) {
+        final item = dashboardItems[index];
+        return GestureDetector(
+          onTap: item.onTap,
+          child: GlassmorphicContainer(
+            width: double.infinity,
+            height: 120,
+            borderRadius: 16,
+            blur: 8,
+            alignment: Alignment.bottomCenter,
+            border: 1,
+            linearGradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                item.color.withOpacity(0.1),
+                item.color.withOpacity(0.05),
+              ],
+            ),
+            borderGradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.white.withOpacity(0.5),
+                Colors.white.withOpacity(0.2),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(item.icon, color: item.color, size: 24),
+                      Text(
+                        '${item.value}/${item.totalValue}',
+                        style: TextStyle(
+                          color: item.color,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        item.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        item.subtitle,
+                        style: const TextStyle(
+                          color: Colors.black54,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
+
+Widget _buildBottomNavigation(BuildContext context) {
+  return BottomNavigationBar(
+    items: [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.task, color: Colors.grey.shade600),
+        label: 'Tasks',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home, color: Colors.blue),
+        label: 'General',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person, color: Colors.grey.shade600),
+        label: 'Profile',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.chat, color: Colors.grey.shade600),
+        label: 'Messenger',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.folder, color: Colors.grey.shade600),
+        label: 'Projects',
+      ),
+    ],
+  );
 }
 
 // Экран папки проекта
